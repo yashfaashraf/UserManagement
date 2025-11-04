6d0fa4f1d3eb1beb4e7009443b5b9e4cc0c890a8
yashfaashraf <yashfaashraf786@gmail.com>
2025-11-05 02:49:58 +0500

    commit css issue

--- frontend-react-ts/src/pages/dashboard/InboxPage.tsx
+++ frontend-react-ts/src/pages/dashboard/InboxPage.tsx
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import { IMessageDto } from '../../types/message.types';
-import axiosInstance from '../../utils/axiosInstance';
-import { MY_MESSAGE_URL } from '../../utils/globalConfig';
-import { toast } from 'react-hot-toast';
-import Spinner from '../../components/general/Spinner';
-import moment from 'moment';
-import { MdInput, MdOutput } from 'react-icons/md';
-import useAuth from '../../hooks/useAuth.hook';
+import { useEffect, useState } from "react";
+import { IMessageDto } from "../../types/message.types";
+import axiosInstance from "../../utils/axiosInstance";
+import { MY_MESSAGE_URL } from "../../utils/globalConfig";
+import { toast } from "react-hot-toast";
+import Spinner from "../../components/general/Spinner";
+import moment from "moment";
+import { MdInput, MdOutput } from "react-icons/md";
+import useAuth from "../../hooks/useAuth.hook";
 
 const InboxPage = () => {
   const { user } = useAuth();
@@ -21,7 +21,7 @@ const InboxPage = () => {
       setMessages(data);
       setLoading(false);
     } catch (error) {
-      toast.error('An Error happened. Please Contact admins');
+      toast.error("An Error happened. Please Contact admins");
       setLoading(false);
     }
   };
@@ -32,34 +32,37 @@ const InboxPage = () => {
 
   if (loading) {
     return (
-      <div className='w-full'>
+      <div className="w-full">
         <Spinner />
       </div>
     );
   }
 
   return (
-    <div className='pageTemplate2'>
-      <h1 className='text-2xl font-bold'>Inbox</h1>
-      <div className='pageTemplate3 items-stretch'>
-        <div className='grid grid-cols-8 p-2 border-2 border-gray-200 rounded-lg'>
+    <div className="pageTemplate2">
+      <h1 className="text-2xl font-bold">Inbox</h1>
+      <div className="pageTemplate3 items-stretch">
+        <div className="grid grid-cols-8 p-2 border-2 border-gray-200 rounded-lg">
           <span>Date</span>
           <span>Type</span>
-          <span className='col-span-4'>Text</span>
+          <span className="col-span-4">Text</span>
           <span>Sender</span>
           <span>Receiver</span>
         </div>
         {messages.map((item) => (
-          <div key={item.id} className='grid grid-cols-8 p-2 border-2 border-gray-200 rounded-lg'>
+          <div
+            key={item.id}
+            className="grid grid-cols-8 p-2 border-2 border-gray-200 rounded-lg"
+          >
             <span>{moment(item.createdAt).fromNow()}</span>
             <span>
               {item.senderUserName === user?.userName ? (
-                <MdOutput className='text-2xl text-purple-500' />
+                <MdOutput className="text-2xl text-purple-500" />
               ) : (
-                <MdInput className='text-2xl text-green-500' />
+                <MdInput className="text-2xl text-green-500" />
               )}
             </span>
-            <span className='col-span-4'>{item.text}</span>
+            <span className="col-span-4">{item.text}</span>
             <span>{item.senderUserName}</span>
             <span>{item.receiverUserName}</span>
           </div>
--- frontend-react-ts/src/pages/dashboard/MyLogsPage.tsx
+++ frontend-react-ts/src/pages/dashboard/MyLogsPage.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from 'react';
-import { ILogDto } from '../../types/log.types';
-import axiosInstance from '../../utils/axiosInstance';
-import { MY_LOGS_URL } from '../../utils/globalConfig';
-import { toast } from 'react-hot-toast';
-import Spinner from '../../components/general/Spinner';
-import moment from 'moment';
+import { useEffect, useState } from "react";
+import { ILogDto } from "../../types/log.types";
+import axiosInstance from "../../utils/axiosInstance";
+import { MY_LOGS_URL } from "../../utils/globalConfig";
+import { toast } from "react-hot-toast";
+import Spinner from "../../components/general/Spinner";
+import moment from "moment";
 
 const MyLogsPage = () => {
   const [myLogs, setMyLogs] = useState<ILogDto[]>([]);
@@ -18,7 +18,7 @@ const MyLogsPage = () => {
       setMyLogs(data);
       setLoading(false);
     } catch (error) {
-      toast.error('An Error happened. Please Contact admins');
+      toast.error("An Error happened. Please Contact admins");
       setLoading(false);
     }
   };
@@ -29,28 +29,31 @@ const MyLogsPage = () => {
 
   if (loading) {
     return (
-      <div className='w-full'>
+      <div className="w-full">
         <Spinner />
       </div>
     );
   }
 
   return (
-    <div className='pageTemplate2'>
-      <h1 className='text-2xl font-bold'>My Logs</h1>
-      <div className='pageTemplate3 items-stretch'>
-        <div className='grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg'>
+    <div className="pageTemplate2">
+      <h1 className="text-2xl font-bold">My Logs</h1>
+      <div className="pageTemplate3 items-stretch">
+        <div className="grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg">
           <span>No</span>
           <span>Date</span>
           <span>Username</span>
-          <span className='col-span-3'>Description</span>
+          <span className="col-span-3">Description</span>
         </div>
         {myLogs.map((item, index) => (
-          <div key={index} className='grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg'>
+          <div
+            key={index}
+            className="grid grid-cols-6 p-2 border-2 border-gray-200 rounded-lg"
+          >
             <span>{index + 1}</span>
             <span>{moment(item.createdAt).fromNow()}</span>
             <span>{item.userName}</span>
-            <span className='col-span-3'>{item.description}</span>
+            <span className="col-span-3">{item.description}</span>
           </div>
         ))}
       </div>
